72c7066f71c9ad43193aeeaf5cca64b689203b18
chriswallace00 <chriswallace00@gmail.com>
2016-10-24 21:42:38 +0100

    tweaking entity detector with nltk

--- text_analysis.py
+++ text_analysis.py
@@ -1,4 +1,3 @@
-from nltk.tag import pos_tag
 import nltk
 import re
 
@@ -12,7 +11,7 @@ class Text_Analyser:
 			self.string_years.append(str(year))
 	# A simple function to extract nouns from text for the purposes of searching
 	def check_for_type(self,text,type):
-		tagged_sent = pos_tag(text.split())
+		tagged_sent = nltk.pos_tag(text.split())
 		type_instances = [word for word,pos in tagged_sent if pos == type]
 		for instance in type_instances:
 			if re.sub('[^A-Za-z0-9]+', '', instance) in self.string_years:
@@ -20,20 +19,32 @@ class Text_Analyser:
 		return type_instances
 
 	def get_subject(self,text):
-		tagged_sent = pos_tag(text.split())
+		tagged_sent = nltk.pos_tag(text.split())
 		type_instances = [word for word,pos in tagged_sent if pos == 'NN']
-		st = NERTagger('stanford-ner/all.3class.distsim.crf.ser.gz', 'stanford-ner/stanford-ner.jar')
 		for sent in nltk.sent_tokenize(text):
 				tokens = nltk.tokenize.word_tokenize(sent)
-				tags = st.tag(tokens)
-				for tag in tags:
-					if tag[1]=='PERSON': 
-						print tag
+				tags = nltk.pos_tag(tokens)
+				print tags
+				# for tag in tags:
+				# 	if tag[1]=='PERSON': 
+				# 		print tag
+
 		return type_instances
 
 	def extract_entities(self,text):
+		entities=[]
 		for sent in nltk.sent_tokenize(text):
 			for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
-				if hasattr(chunk, 'node'):
-					print chunk.node, ' '.join(c[0] for c in chunk.leaves())
+				if len(chunk):
+					if 'GPE' in str(chunk):
+						results= str(chunk[0]).replace("(","").replace(")","").replace("'", "").split(',')
+						entities.append(results[0])
+		return entities
+				# if hasattr(chunk, 'node'):
+				#  	for c in chunk.leaves():
+				#  		if c[0] is 'GPE':
+				#  			print c
+				# 		if 'GPE' in c:
+				# 			print c	
+					#print chunk.node, ' '.join(c[0] for c in chunk.leaves())
 
